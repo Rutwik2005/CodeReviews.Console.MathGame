d8e9d9cb6d62f8d64e9707221f36a5dbf27ce86f
Rutwik Vaidya <rutwik2005vaidya@gmail.com>
2024-09-30 15:55:12 +0530

    All The Required changes done Aligned according to requirements.

---
+++ MathGame.Rutwik2005/MathGame.Rutwik2005/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace MathGame.Rutwik2005.Models
+{ 
+    internal class Game
+{
+    //private int score;
+    //public int Score
+    //{
+    //    get { return score; }
+    //    set { score = value; }
+    //}
+    public DateTime Date { get; set; }
+    public int Score { get; set; }
+    public string Type { get; set; }
+
+}
+
+}
