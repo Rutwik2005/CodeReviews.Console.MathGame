f1614dfff630a401bbdce8d47d2bdf166110d34e
Rutwik Vaidya <rutwik2005vaidya@gmail.com>
2024-09-30 15:58:55 +0530

    Deleted commented out code

--- MathGame.Rutwik2005/MathGame.Rutwik2005/Models/Game.cs
+++ MathGame.Rutwik2005/MathGame.Rutwik2005/Models/Game.cs
@@ -2,12 +2,7 @@
 { 
     internal class Game
 {
-    //private int score;
-    //public int Score
-    //{
-    //    get { return score; }
-    //    set { score = value; }
-    //}
+    
     public DateTime Date { get; set; }
     public int Score { get; set; }
     public string Type { get; set; }
